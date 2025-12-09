7f359be279c297b0b395710ceb0609a208144299
Adrian Dev <artbenitezDev@gmail.com>
2025-12-09 20:41:11 -0300

    with login

---
+++ login/login.js
@@ -0,0 +1,16 @@
+async function  logear(){
+    let user=document.getElementById("user").value;
+    let pass=document.getElementById("pass").value;
+    let direccionUrl="https://script.google.com/macros/s/AKfycbx-YwE7fkQKIyiQV13JPs0iIxRWw-nohtciTnR0Gb2G_ef6qtWSHSDEro_ipWeiBnTtKg/exec";
+
+
+  let resp= await fetch(direccionUrl,{
+        method:'POST', headers: {
+    "Content-Type": "text/plain"  // 👈 TRUCO CLAVE: NO HAY OPTIONS
+  },
+  body: JSON.stringify({"type":"login","usuario":user,"contra":pass})
+    })
+    
+     const texto = await resp.text(); // <-- aquí está la respuesta REAL
+  console.log("Respuesta del backend:", texto);
+}
