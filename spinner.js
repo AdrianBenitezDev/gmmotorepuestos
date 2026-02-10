0212efa68e229a9814bef64a1a59911dc33e5468
Adrian Dev <artbenitezDev@gmail.com>
2026-02-10 10:00:32 -0300

    change

--- spinner.js
+++ spinner.js
@@ -2,11 +2,13 @@ export function spinnerBusqueda(boleano){
   console.log("spinnerBusqueda:")
   console.log(boleano)
 
-  
+
     let spin=document.getElementById("overlayBusqueda")
     if(boleano){
         spin.style.display="flex";
+        console.log("colocamos flex en spinnerBusqueda")
     }else{
+      console.log("colocamos none en spinnerBusqueda")
          spin.style.display="none";
     }
 }
